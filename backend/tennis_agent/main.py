5f84510f158933d738dd48c1fa70dcc1577cc112
mshen5390 <140674495+mshen5390@users.noreply.github.com>
2025-07-18 16:10:50 -0400

    initial commit

---
+++ backend/tennis_agent/main.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from langchain_openai import ChatOpenAI
+from langchain_anthropic import ChatAnthropic
+
+load_dotenv()
+
+llm = ChatOpenAI(
+    model="gpt-4o-mini",
+)
+
+response = llm.invoke("What is the capital of France?")
+print(response)
